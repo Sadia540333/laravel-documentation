77b855f3f1de5a4ed5a4dd136d537e1aa2853a02
sadia554466 <pue.khulna@gmail.com>
2023-10-22 12:00:06 +0600

    laravel-documentation

---
+++ main.js
@@ -0,0 +1,91 @@
+let editor = document.querySelector("#editor");
+
+ace.edit(editor, {
+  theme: "ace/theme/cobalt",
+  mode: "ace/mode/javascript",
+});
+let controller1 = document.querySelector("#controller1");
+
+ace.edit(controller1, {
+  theme: "ace/theme/cobalt",
+  mode: "ace/mode/javascript",
+});
+let controller2 = document.querySelector("#controller2");
+
+ace.edit(controller2, {
+  theme: "ace/theme/cobalt",
+  mode: "ace/mode/javascript",
+});
+let controller3 = document.querySelector("#controller3");
+
+ace.edit(controller3, {
+  theme: "ace/theme/cobalt",
+  mode: "ace/mode/javascript",
+});
+let controller4 = document.querySelector("#controller4");
+
+ace.edit(controller4, {
+  theme: "ace/theme/cobalt",
+  mode: "ace/mode/javascript",
+});
+let controller5 = document.querySelector("#controller5");
+
+ace.edit(controller5, {
+  theme: "ace/theme/cobalt",
+  mode: "ace/mode/javascript",
+});
+
+// ===CRUD With Image Start===
+let command1 = document.querySelector("#command1");
+
+ace.edit(command1, {
+  theme: "ace/theme/cobalt",
+  mode: "ace/mode/javascript",
+});
+let command2 = document.querySelector("#command2");
+
+ace.edit(command2, {
+  theme: "ace/theme/cobalt",
+  mode: "ace/mode/javascript",
+});
+let command3 = document.querySelector("#command3");
+
+ace.edit(command3, {
+  theme: "ace/theme/cobalt",
+  mode: "ace/mode/javascript",
+});
+let command4 = document.querySelector("#command4");
+
+ace.edit(command4, {
+  theme: "ace/theme/cobalt",
+  mode: "ace/mode/javascript",
+});
+let command5 = document.querySelector("#command5");
+
+ace.edit(command5, {
+  theme: "ace/theme/cobalt",
+  mode: "ace/mode/javascript",
+});
+// CURD Multhiauth 
+let multhiauth = document.querySelector("#multhiauth");
+
+ace.edit(multhiauth, {
+  theme: "ace/theme/cobalt",
+  mode: "ace/mode/javascript",
+});
+// ===CRUD With Image Start===
+
+// ===CRUD Without Image Start===
+let command01 = document.querySelector("#command01");
+
+ace.edit(command01, {
+  theme: "ace/theme/cobalt",
+  mode: "ace/mode/javascript",
+});
+let command02 = document.querySelector("#command02");
+
+ace.edit(command02, {
+  theme: "ace/theme/cobalt",
+  mode: "ace/mode/javascript",
+});
+// ===CRUD Without Image Start===
